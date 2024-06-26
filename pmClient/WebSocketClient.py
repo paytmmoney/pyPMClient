99b5cc165894a73aa8126746cf47134a72a217b8
sachin3.yadav <sachin3.yadav@paytmmoney.com>
2024-06-26 09:04:52 +0530

    revert safe unpack

--- pmClient/WebSocketClient.py
+++ pmClient/WebSocketClient.py
@@ -208,11 +208,6 @@ class WebSocketClient(Constants):
     def unpack(self, binary, start, end, byte_format="I"):
         """Unpack binary data as given data type format."""
         return struct.unpack("<" + byte_format, binary[start:end])[0]
-    
-    def safe_unpack(self, binary, start, end, byte_format):
-        if end - start < struct.calcsize("<" + byte_format):
-            return struct.unpack("<" + "I", binary[start:end])[0]
-        return struct.unpack("<" + byte_format, binary[start:end])[0]
 
     def parse_binary(self, buffer_packet):
         """
